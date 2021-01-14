67baf4e2f86e487a02a4d519a4bc79b4807bd6a0
Blazed <56462618+blazed95@users.noreply.github.com>
2021-01-14 10:58:31 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(express.json());
-//app.use('/api', router) // Mount the router to the app /api
+
 app.use('/uploads', express.static("uploads"));
 app.use(express.json());
 //This is a middleware function with no mount path. The function is executed every time the app receives a request.
@@ -38,27 +38,12 @@ app.use(cors({
 require("./config-server/mongoose.js")(app); //Adding this will connect to our database when ever our Nodejs server is running.
 require('./routerHandler')(app)
 require('dotenv').config()
-/*router.post('/stripe/charge', postCharge) ///the stripe/charge is the endpoint and the postCharge handles the requests to this route
-router.all('*', (_, res) =>
-    res.json({ message: 'please make a POST request to /stripe/charge' })
-)
-*/
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Waaas up buddy. Welcome to my Eshop'
     });
 });
 
-
-
-//app.use(express.urlencoded());
-//Build
-/*app.use(express.static(path.join(__dirname, '../build'))) // Example: var x = path.join('Users', 'Refsnes', 'demo_path.js'); ---- console.log(x) = Users\Refsnes\demo_path.js
-
-
-// This will serve the frontend in production.
-app.get('*', (_, res) => {
-    res.sendFile(path.resolve(__dirname, '../build/index.html'))  //handle all GET requests by serving the index.html file located in the /build folder
-})*/
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
